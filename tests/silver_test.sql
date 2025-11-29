c8180753f43bf6ef6b946dc558270adddf77d738
liam29102003 <91581132+liam29102003@users.noreply.github.com>
2025-11-29 03:22:13 -0800

    Create silver_test.sql

---
+++ tests/silver_test.sql
@@ -0,0 +1,121 @@
+
+-- CHECK For Nulls or Duplicates in Primary Key
+-- Expectation: No Result
+
+SELECT
+cst_id,
+COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- CHECK For unwanted spaces
+-- Expectation: No Result
+
+SELECT cst_firstname
+FROM silver.crm_cust_info 
+WHERE cst_firstname != TRIM(cst_firstname);
+
+SELECT cst_lastname
+FROM silver.crm_cust_info 
+WHERE cst_lastname != TRIM(cst_lastname);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info;
+
+SELECT DISTINCT cst_material_status
+FROM silver.crm_cust_info;
+
+-- Check for nulls or duplicates in primary key
+-- Expectation: No Result
+
+SELECT
+prd_id,
+COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+Having COUNT(*) > 1 or prd_id IS NULL
+
+-- Check for unwanted spaces
+-- Expectation: No Results
+
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+-- Check for nulls or negative numbers
+-- Expectation: No Results
+
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+-- Data Standardization & Consistency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info;
+
+--Check for invalid date orders
+-- Expectation: No Results
+SELECT * 
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_data
+
+-- Check for invalid dates
+
+SELECT
+sls_order_dt
+FROM silver.crm_sales_details
+WHERE sls_order_dt <= 0 OR LEN(sls_order_dt) != 8 OR sls_order_dt > 20500101 OR sls_order_dt < 19000101
+
+--Check for Invalid Date Orders
+SELECT
+*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+--- CHECK Data Consistency: Between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero, or negative
+SELECT
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <=0 OR sls_quantity <=0 OR sls_price <= 0 
+ORDER BY sls_sales,sls_quantity, sls_price
+
+SELECT * FROM silver.crm_sales_details
+
+-- Indentify out-of-range Dates
+
+SELECT DISTINCT
+bdate
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+--- Data Standardization & Consistency
+SELECT DISTINCT gen
+FROM silver.erp_cust_az12
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cntyr FROM silver.erp_loc_a101 ORDER BY cntyr 
+
+--check for unwanted spaces
+SELECT *  FROM silver.erp_px_cat_g1v2
+WHERE  cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance)
+
+-- Data Standardization & Consistency
+SELECT DISTINCT
+cat
+FROM  silver.erp_px_cat_g1v2
+
+SELECT DISTINCT
+subcat
+FROM  silver.erp_px_cat_g1v2
+
+SELECT DISTINCT
+maintenance
+FROM  silver.erp_px_cat_g1v2
